db56d5c889e7bc6fbc418c236b3a3966532b93b0
koosha <107479207+KooshaNaji@users.noreply.github.com>
2025-04-04 19:07:21 +0330

    Delete feature engeeniring directory

--- feature engeeniring/bank-marketing-project/notebooks/koosha_preprocessing.py
+++
@@ -1,9 +0,0 @@
-class PreprocessingKoosha:
-    def __init__(self, data_frame):
-        self.df = data_frame
-
-    def fill_with_mode(self, target_column):
-        self.df[target_column].fillna(self.df[target_column].mode()[0], inplace=True)
-
-    def get_data(self):
-        return self.df
